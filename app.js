cc5e03d9a8ae8e21946f8d7204c04bb28e5887f8
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2019-12-27 05:10:20 +0530

    Added the global and local components

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-Vue.component("plan", {
+//Local component
+let planComponent = {
   template: "#plan-template",
   // props: ["name"]
   props: {
     name: {
       type: String,
-      default: 'Basic',
+      default: "Basic",
       required: true
     }
+  }
+};
+
+//Global Component //If the root component doesnt have the parent to be registered as global component then define it in root instance
+Vue.component("plan-picker", {
+  template: "#plan-picker-template",
+  components: {
+    plan: planComponent
+  },
+  data() {
+    return {
+      plans: [
+        {
+          key: 1,
+          title: "The Rock"
+        },
+        {
+          key: 2,
+          title: "The Single"
+        },
+        {
+          key: 3,
+          title: "The Couple"
+        }
+      ]
+    };
   }
 });
 
 new Vue({
-  el: "#app",
-  data: {
-    plans: ["The Single", "The Couple", "The Rock"]
-  }
+  el: "#app"
 });

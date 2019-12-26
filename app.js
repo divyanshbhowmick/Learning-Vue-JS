2d79cbe34d66644bdd1aa91e3090841ab786306d
divyanshbhowmick <divyansh.bhowmick3@gmail.com>
2019-12-27 01:10:22 +0530

    Addded the comment about data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Vue.component("click-counter", {
   template: '#click-counter-template',
-  data() {
+  data() { // Data must be an function so that each instance can maintain an seperate copy of it!
     return {
       count: 0
     };
